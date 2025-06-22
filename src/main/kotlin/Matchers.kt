fc4010db809e53c23bdf45f3590a6281653b58e1
Igor Shaula <igor.shaula@gmail.com>
2025-06-22 21:47:12 +0300

    + String.normalize() and used it in Matchers.kt for optimization

--- src/main/kotlin/Matchers.kt
+++ src/main/kotlin/Matchers.kt
@@ -1,18 +1,16 @@
 package org.igor_shaula
 
-fun isEnglishUSNameGroup1(line: String): Boolean {
-    // Remove all whitespace and then compare with the normalized pattern
-    val normalizedLine = line.replace("\\s+".toRegex(), "")
-    return normalizedLine == """name[Group1]="English(US)";"""
-}
+fun isEnglishUSNameGroup1(line: String) = line.normalize() == """name[Group1]="English(US)";"""
 
 fun isKeyTilda(line: String): Boolean {
-    // Remove all whitespace and then compare with the normalized pattern
-    val normalizedLine = line.replace("\\s+".toRegex(), "")
+    val normalizedLine = line.normalize()
     return normalizedLine.startsWith("""key<TLDE>{[""") && normalizedLine.contains("]};")
 }
 
 fun isKeyStartingWithA(line: String): Boolean {
-    val normalizedLine = line.replace("\\s+".toRegex(), "")
+    val normalizedLine = line.normalize()
     return normalizedLine.startsWith("""key<A""") && normalizedLine.contains("]};")
 }
+
+// Remove all whitespace and then compare with the normalized pattern
+private fun String.normalize() = replace("\\s+".toRegex(), "")
