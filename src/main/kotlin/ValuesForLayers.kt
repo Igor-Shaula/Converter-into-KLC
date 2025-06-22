d07e6c92d7b73cfd7550c63bbe726f98ecffc687
Igor Shaula <igor.shaula@gmail.com>
2025-06-22 17:33:16 +0300

    + ValuesForLayers data class

---
+++ src/main/kotlin/ValuesForLayers.kt
@@ -0,0 +1,8 @@
+package org.igor_shaula
+
+data class ValuesForLayers(
+    val layer1: String, // base
+    val layer2: String, // + Shift
+    val layer3: String, // + AltGr
+    val layer4: String // + AltGr & Shift
+)
