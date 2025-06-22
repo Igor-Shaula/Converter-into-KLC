0283cc23d99230e6bcd147860cd5bafd6e8bb69e
Igor Shaula <igor.shaula@gmail.com>
2025-06-22 22:06:40 +0300

    + Constants.kt & moved all hardcoded symbols there

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,16 @@
+package org.igor_shaula
+
+const val REGEX_FOR_REMOVING_BLANKS = "\\s+"
+const val EMPTY_STRING = ""
+const val EQUALS = '='
+const val SEMICOLON = ';'
+const val DOUBLE_QUOTE = '"'
+
+const val NAME_GROUP_1 = "name[Group1]"
+const val ENGLISH_US = "English(US)"
+
+const val KEY_TILDE = "key<TLDE>"
+const val KEY_A_BEGINNING = "key<A"
+
+const val OPENING_BRACKETS = "{["
+const val CLOSING_BRACKETS = "]}"
--- src/main/kotlin/Matchers.kt
+++ src/main/kotlin/Matchers.kt
@@ -1,16 +1,17 @@
 package org.igor_shaula
 
-fun isEnglishUSNameGroup1(line: String) = line.normalize() == """name[Group1]="English(US)";"""
+fun isEnglishUSNameGroup1(line: String) =
+    line.normalize() == NAME_GROUP_1 + EQUALS + DOUBLE_QUOTE + ENGLISH_US + DOUBLE_QUOTE + SEMICOLON
 
 fun isKeyTilda(line: String): Boolean {
     val normalizedLine = line.normalize()
-    return normalizedLine.startsWith("""key<TLDE>{[""") && normalizedLine.contains("]};")
+    return normalizedLine.startsWith(KEY_TILDE + OPENING_BRACKETS) && normalizedLine.contains(CLOSING_BRACKETS + SEMICOLON)
 }
 
 fun isKeyStartingWithA(line: String): Boolean {
     val normalizedLine = line.normalize()
-    return normalizedLine.startsWith("""key<A""") && normalizedLine.contains("]};")
+    return normalizedLine.startsWith(KEY_A_BEGINNING) && normalizedLine.contains(CLOSING_BRACKETS + SEMICOLON)
 }
 
 // Remove all whitespace and then compare with the normalized pattern
-private fun String.normalize() = replace("\\s+".toRegex(), "")
+private fun String.normalize() = replace(REGEX_FOR_REMOVING_BLANKS.toRegex(), EMPTY_STRING)
