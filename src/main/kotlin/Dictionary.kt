9ee92509b74c19af2da2600f138287d102b1db41
Igor Shaula <igor.shaula@gmail.com>
2025-06-27 00:17:40 +0300

    modified xkbToWindowsScancode to contain String as a value - suitable for KLC files

--- src/main/kotlin/Dictionary.kt
+++ src/main/kotlin/Dictionary.kt
@@ -14,66 +14,66 @@ fun parseKeySymDefinition(input: String): Pair<String, String>? {
     }
 }
 
-val xkbToWindowsScancode: Map<String, Int> = mapOf(
-    "AE01" to 0x02, // 1
-    "AE02" to 0x03, // 2
-    "AE03" to 0x04, // 3
-    "AE04" to 0x05, // 4
-    "AE05" to 0x06, // 5
-    "AE06" to 0x07, // 6
-    "AE07" to 0x08, // 7
-    "AE08" to 0x09, // 8
-    "AE09" to 0x0A, // 9
-    "AE10" to 0x0B, // 0
-    "AE11" to 0x0C, // -
-    "AE12" to 0x0D, // =
+val xkbToWindowsScancode: Map<String, String> = mapOf(
+    "AE01" to "02", // 1
+    "AE02" to "03", // 2
+    "AE03" to "04", // 3
+    "AE04" to "05", // 4
+    "AE05" to "06", // 5
+    "AE06" to "07", // 6
+    "AE07" to "08", // 7
+    "AE08" to "09", // 8
+    "AE09" to "0A", // 9
+    "AE10" to "0B", // 0
+    "AE11" to "0C", // -
+    "AE12" to "0D", // =
 
-    "AD01" to 0x10, // Q
-    "AD02" to 0x11, // W
-    "AD03" to 0x12, // E
-    "AD04" to 0x13, // R
-    "AD05" to 0x14, // T
-    "AD06" to 0x15, // Y
-    "AD07" to 0x16, // U
-    "AD08" to 0x17, // I
-    "AD09" to 0x18, // O
-    "AD10" to 0x19, // P
-    "AD11" to 0x1A, // [
-    "AD12" to 0x1B, // ]
+    "AD01" to "10", // Q
+    "AD02" to "11", // W
+    "AD03" to "12", // E
+    "AD04" to "13", // R
+    "AD05" to "14", // T
+    "AD06" to "15", // Y
+    "AD07" to "16", // U
+    "AD08" to "17", // I
+    "AD09" to "18", // O
+    "AD10" to "19", // P
+    "AD11" to "1A", // [
+    "AD12" to "1B", // ]
 
-    "AC01" to 0x1E, // A
-    "AC02" to 0x1F, // S
-    "AC03" to 0x20, // D
-    "AC04" to 0x21, // F
-    "AC05" to 0x22, // G
-    "AC06" to 0x23, // H
-    "AC07" to 0x24, // J
-    "AC08" to 0x25, // K
-    "AC09" to 0x26, // L
-    "AC10" to 0x27, // ;
-    "AC11" to 0x28, // '
+    "AC01" to "1E", // A
+    "AC02" to "1F", // S
+    "AC03" to "20", // D
+    "AC04" to "21", // F
+    "AC05" to "22", // G
+    "AC06" to "23", // H
+    "AC07" to "24", // J
+    "AC08" to "25", // K
+    "AC09" to "26", // L
+    "AC10" to "27", // ;
+    "AC11" to "28", // '
 
-    "AB01" to 0x2C, // Z
-    "AB02" to 0x2D, // X
-    "AB03" to 0x2E, // C
-    "AB04" to 0x2F, // V
-    "AB05" to 0x30, // B
-    "AB06" to 0x31, // N
-    "AB07" to 0x32, // M
-    "AB08" to 0x33, // ,
-    "AB09" to 0x34, // .
-    "AB10" to 0x35, // /
+    "AB01" to "2C", // Z
+    "AB02" to "2D", // X
+    "AB03" to "2E", // C
+    "AB04" to "2F", // V
+    "AB05" to "30", // B
+    "AB06" to "31", // N
+    "AB07" to "32", // M
+    "AB08" to "33", // ,
+    "AB09" to "34", // .
+    "AB10" to "35", // /
 
-    "TLDE" to 0x29, // ` ~
-    "BKSP" to 0x0E, // Backspace
-    "TAB"  to 0x0F, // Tab
-    "CAPS" to 0x3A, // Caps Lock
-    "RTRN" to 0x1C, // Enter
-    "LFSH" to 0x2A, // Left Shift
-    "RTSH" to 0x36, // Right Shift
-    "LCTL" to 0x1D, // Left Ctrl
-    "RCTL" to 0x61, // Right Ctrl (approx.)
-    "LALT" to 0x38, // Left Alt
-    "RALT" to 0x64, // Right Alt / AltGr
-    "SPCE" to 0x39  // Space
+    "TLDE" to "29", // ` ~
+    "BKSP" to "0E", // Backspace
+    "TAB" to "0F", // Tab
+    "CAPS" to "3A", // Caps Lock
+    "RTRN" to "1C", // Enter
+    "LFSH" to "2A", // Left Shift
+    "RTSH" to "36", // Right Shift
+    "LCTL" to "1D", // Left Ctrl
+    "RCTL" to "61", // Right Ctrl (approx.)
+    "LALT" to "38", // Left Alt
+    "RALT" to "64", // Right Alt / AltGr
+    "SPCE" to "39"  // Space
 )
