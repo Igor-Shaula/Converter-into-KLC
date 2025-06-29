f5c2454a587cdaca54c6794ee61a1d435ccf28c9
Igor Shaula <igor.shaula@gmail.com>
2025-06-29 13:55:33 +0300

    adjusted vkValue & capitalized creation to align the result file better

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -73,8 +73,8 @@ fun composeKlcFile() {
     resultFile.writeText(KLC_FILE_PREFIX.replace(LF, CR_LF), charset = Charsets.UTF_16)
     windowsEssence.forEach { (key, value) ->
         val scValue = key?.lowercase()
-        val vkValue = getVkValueByScValue(key?.lowercase()) ?: value.layer1.uppercase()
-        val capitalized = getCapitalized(vkValue)
+        val vkValue = getVkValueByScValue(key?.lowercase()) ?: (value.layer1.uppercase() + "\t")
+        val capitalized = getCapitalized(value.layer1)
         val (layer1, layer2, layer3, layer4) = value.adaptForWindows()
         resultFile.appendText(
             "$scValue\t$vkValue\t$capitalized\t$layer1\t$layer2\t-1\t$layer3\t$layer4$CR_LF", charset = Charsets.UTF_16
