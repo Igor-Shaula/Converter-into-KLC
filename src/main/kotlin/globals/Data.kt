2cfcee32799dcbbd78ef16436aaa065d21ead875
Igor Shaula <igor.shaula@gmail.com>
2025-07-02 02:15:30 +0300

    + 2 data containers for processing aliases for keycodes

--- src/main/kotlin/globals/Data.kt
+++ src/main/kotlin/globals/Data.kt
@@ -3,8 +3,10 @@ package org.igor_shaula.globals
 import org.igor_shaula.logic.ValuesForLayers
 
 internal var isInsideLanguageBlock = false
+internal var isInsideKeycodesBlock = false
 
 internal val x11SymbolsDictionary = mutableMapOf<String, String>()
+internal val x11LatAliasesDictionary = mutableMapOf<String, String>()
 
 internal val x11Essence = mutableMapOf<String, ValuesForLayers>()
 
