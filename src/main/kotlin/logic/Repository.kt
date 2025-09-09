9bbd435e744de21b72ff8920f96b3a25a3e5cf5b
Igor Shaula <igor.shaula@gmail.com>
2025-09-09 23:55:19 +0300

    moved getUnicodeSymbolFromKeysym() closer to the place of invocation

--- src/main/kotlin/logic/Repository.kt
+++ src/main/kotlin/logic/Repository.kt
@@ -1,5 +1,7 @@
 package org.igor_shaula.logic
 
+import org.igor_shaula.globals.HEX_RADIX
+import org.igor_shaula.globals.keysymToUnicodeMapping
 import org.igor_shaula.globals.xkbToWindowsScancode
 import org.igor_shaula.utils.l
 
@@ -29,6 +31,19 @@ class Repository {
             symbolsDictionary[it.key] = getUnicodeSymbolFromKeysym(it.value)
         }
         l("standard symbols dictionary: $symbolsDictionary")
+    }
+
+    private fun getUnicodeSymbolFromKeysym(keysym: String): Char? {
+        try {
+            val keySymValue = keysym.toInt(HEX_RADIX)
+            // X11 Keysym to Unicode mapping for Cyrillic
+            val unicodeValue = keysymToUnicodeMapping[keySymValue] ?: keySymValue
+            // fallback for non-Cyrillic or direct Unicode values
+            return unicodeValue.toChar()
+        } catch (e: NumberFormatException) {
+            l("Invalid input: '$keysym'. NumberFormatException: ${e.message}")
+            return null
+        }
     }
 
     /**
