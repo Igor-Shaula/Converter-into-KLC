6390e9b83b65a14b65c2cd2ab2ae50f70bf03322
Igor Shaula <igor.shaula@gmail.com>
2025-09-11 23:42:54 +0300

    + generateBuildConfig in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "org.igor_shaula"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -15,4 +15,36 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val generateBuildConfig by tasks.registering {
+    val outputDir = layout.buildDirectory.dir("generated/source/buildConfig/kotlin").get().asFile
+    val packageName = "org.igor_shaula" // <-- change to your package
+
+    inputs.property("version", project.version)
+    outputs.dir(outputDir)
+
+    doLast {
+        val file = File(outputDir, "${packageName.replace('.', '/')}/BuildConfig.kt")
+        file.parentFile.mkdirs()
+        file.writeText(
+            """
+            package $packageName
+
+            object BuildConfig {
+                const val VERSION = "${project.version}"
+            }
+            """.trimIndent()
+        )
+    }
+}
+
+kotlin {
+    sourceSets.main {
+        kotlin.srcDir(layout.buildDirectory.dir("generated/source/buildConfig/kotlin"))
+    }
+}
+
+tasks.named("compileKotlin") {
+    dependsOn(generateBuildConfig)
 }
