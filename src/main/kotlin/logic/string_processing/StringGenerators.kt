5c6ed99ab438e795687aba8e17bcb1b327fe2493
Igor Shaula <igor.shaula@gmail.com>
2025-09-23 00:38:07 +0300

    made all internal functions public in StringGenerators for simplicity

--- src/main/kotlin/logic/string_processing/StringGenerators.kt
+++ src/main/kotlin/logic/string_processing/StringGenerators.kt
@@ -8,16 +8,16 @@ private val xkbSymbolsRegex = Regex.XKB_SYMBOLS_FINDING_PATTERN.toRegex()
 private val xkbKeycodesRegex = Regex.XKB_KEYCODES_FINDING_PATTERN.toRegex()
 
 // removes all whitespace in the given line
-internal fun String.clearAllBlanks() = replace(whitespacesRegex, Str.EMPTY)
+fun String.clearAllBlanks() = replace(whitespacesRegex, Str.EMPTY)
 
 // extracts the section name:
-internal fun getXkbSymbolsSectionName(line: String): String? = xkbSymbolsRegex.find(line)?.groupValues?.get(1)
+fun getXkbSymbolsSectionName(line: String): String? = xkbSymbolsRegex.find(line)?.groupValues?.get(1)
 
-internal fun getXkbKeycodesSectionName(line: String): String? = xkbKeycodesRegex.find(line)?.groupValues?.get(1)
+fun getXkbKeycodesSectionName(line: String): String? = xkbKeycodesRegex.find(line)?.groupValues?.get(1)
 
-internal fun getCapitalizedValue(base: String) = if (base.length == 1 && base.first().isLetter()) 1 else 0
+fun getCapitalizedValue(base: String) = if (base.length == 1 && base.first().isLetter()) 1 else 0
 
-internal fun String.mapToKeysym(repository: Repository): String = when {
+fun String.mapToKeysym(repository: Repository): String = when {
     this.isBlank() -> Str.EMPTY // should not ever happen
     this.length == 1 -> this // must be located before the next case with starting-with-U
     this.isUnicodeNumber() -> this.substring(1).lowercase() // because the length of the char U is "1"
@@ -32,20 +32,19 @@ internal fun String.mapToKeysym(repository: Repository): String = when {
     }
 }
 
-internal fun String.filterCommands() =
-    if (this.lowercase().startsWith(Sym.BEGINNING_OF_COMMAND_SYMBOL)) Str.EMPTY else this
+fun String.filterCommands() = if (this.lowercase().startsWith(Sym.BEGINNING_OF_COMMAND_SYMBOL)) Str.EMPTY else this
 
-internal fun String.filterMissingKeysyms() = if (this == X11.NOSYMBOL.lowercase() || is0x100KeysymCode(this)) {
+fun String.filterMissingKeysyms() = if (this == X11.NOSYMBOL.lowercase() || is0x100KeysymCode(this)) {
     Str.EMPTY
 } else this
 
 private fun is0x100KeysymCode(s: String) =
     s.startsWith(X11.EXTENDED_CODE_PREFIX_HEX) && s.length > X11.UNICODE_VALUE_LENGTH
 
-internal fun String.getX11KeycodeBaseValue() = this.substring(X11.EXTENDED_CODE_PREFIX.length)
+fun String.getX11KeycodeBaseValue() = this.substring(X11.EXTENDED_CODE_PREFIX.length)
 
-internal fun String.getKeyNameStartingWithA() =
+fun String.getKeyNameStartingWithA() =
     substring(X11.KEY_A_BEGINNING.length - 1, X11.KEY_A_BEGINNING.length + 3) // 4 in total
 
-internal fun String.getKeyNameStartingWithLat() =
+fun String.getKeyNameStartingWithLat() =
     substring(X11.KEY_LAT_BEGINNING.length - 3, X11.KEY_LAT_BEGINNING.length + 1) // 4 in total
